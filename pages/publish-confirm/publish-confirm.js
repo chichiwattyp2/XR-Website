9bd3d78a43f12d899c4bb074247f40a93c144a2d
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 20:41:20 +0200

    Update publish-confirm.js

--- pages/publish-confirm/publish-confirm.js
+++ pages/publish-confirm/publish-confirm.js
@@ -1,10 +1,15 @@
 // pages/publish-confirm/publish-confirm.js
 
 async function publishConfirm() {
-  let response = await fetch(`https://gatekeeper-arjsstudio.fly.dev/authenticate/${queryDict.code}`);
+  if (typeof window === 'undefined' || typeof Package === 'undefined') {
+    return; // prevent SSR from running this
+  }
+
+  let response = await fetch(
+    `https://gatekeeper-arjsstudio.fly.dev/authenticate/${queryDict.code}`
+  );
   response = await response.json();
 
-  // renamed from package → pkg
   const pkg = new Package(window.session);
   const pagesUrl = await pkg.serve({
     packageType: 'github',
