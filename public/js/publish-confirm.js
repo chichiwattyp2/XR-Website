8d5d26f101293c826d40c1b434419681a9b21189
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 21:02:20 +0200

    Update publish-confirm.js

--- public/js/publish-confirm.js
+++ public/js/publish-confirm.js
@@ -1,12 +1,23 @@
-'use client';
-import Script from 'next/script';
+// pages/publish-confirm/publish-confirm.js
 
-export default function PublishConfirmPage() {
-  return (
-    <main>
-      <h1>Publish Confirm</h1>
-      <Script src="/js/utils.js" strategy="beforeInteractive" />
-      <Script src="/js/publish-confirm.js" strategy="afterInteractive" />
-    </main>
+async function publishConfirm() {
+  if (typeof window === 'undefined' || typeof Package === 'undefined') {
+    return; // prevent SSR from running this
+  }
+
+  let response = await fetch(
+    `https://gatekeeper-arjsstudio.fly.dev/authenticate/${queryDict.code}`
   );
+  response = await response.json();
+
+  const pkg = new Package(window.session);
+  const pagesUrl = await pkg.serve({
+    packageType: 'github',
+    token: response.token, // required, must be an OAuth2 token
+    message: 'first commit for WebAR!', // optional
+  });
+
+  console.log('Published pages URL:', pagesUrl);
 }
+
+publishConfirm();
