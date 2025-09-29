b018cf9fcf066da2193eda190ce670551350431a
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 20:56:58 +0200

    Create publish-confirm.js

---
+++ pages/publish-confirm.js
@@ -0,0 +1,11 @@
+'use client';
+import Script from 'next/script';
+
+export default function PublishConfirmPage() {
+  return (
+    <main>
+      <h1>Publish Confirm</h1>
+      <Script src="/js/publish-confirm.js" strategy="afterInteractive" />
+    </main>
+  );
+}
