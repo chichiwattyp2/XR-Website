58d0d2707b8dff6b688d9450e3424add9a4f631a
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 20:40:34 +0200

    Update location.js

--- pages/location/location.js
+++ pages/location/location.js
@@ -1,18 +1,19 @@
 // pages/location/location.js
 
-// create the package (renamed to pkg)
-const pkg = new Package({
-  arType: 'location',
-  assetType: window.assetType, // image/audio/video/3d
-  assetFile: window.assetFile,
-  assetParam: window.assetParam
-});
+if (typeof window !== 'undefined' && typeof Package !== 'undefined') {
+  const pkg = new Package({
+    arType: 'location',
+    assetType: window.assetType, // image/audio/video/3d
+    assetFile: window.assetFile,
+    assetParam: window.assetParam,
+  });
 
-pkg.serve({ packageType: 'zip' }).then((base64) => {
-  const link = document.createElement('a');
-  link.href = `data:application/zip;base64,${base64}`;
-  link.download = 'ar.zip';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-});
+  pkg.serve({ packageType: 'zip' }).then((base64) => {
+    const link = document.createElement('a');
+    link.href = `data:application/zip;base64,${base64}`;
+    link.download = 'ar.zip';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+}
